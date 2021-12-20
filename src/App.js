9178fd5a6bed7539d84b9ef4aec53d5293d1c6aa
Preetam Daila <preetam.daila@gmail.com>
2021-12-20 23:52:20 +0530

    added hover-text added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import "./styles.css"
-import { TwoVideoContainer } from "./TwoVideoContainer"
+import "./styles.css";
+import { TwoVideoContainer } from "./TwoVideoContainer";
 
 export default function App() {
     return (
         <div className="App">
             <TwoVideoContainer
                 screenRecording={
-                    "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4"
+                    "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/sample-mp4-file.mp4"
+                    // "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/Sample-Video-File-For-Testing.mp4"
                 }
                 audioVideoRecording={
                     "https://jsoncompare.org/LearningContainer/SampleFiles/Video/MP4/sample-mp4-file.mp4"
                 }
             />
         </div>
-    )
+    );
 }
